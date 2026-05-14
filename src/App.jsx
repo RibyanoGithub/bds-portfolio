fcebf44d0c20e8ff96e512a82e9bd864c077a177
Johnfel Pascual <jeffpaskie@gmail.com>
2026-05-14 22:16:44 +0800

    sikam kumet

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
-        BADS TOOOOO!
+        BADS TOOOOO! Badeng se e jey
       </h1>
     </div>
   )
